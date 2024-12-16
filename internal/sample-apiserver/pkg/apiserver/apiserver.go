bc459836b42d0a17dfcd665e405855acff03c67f
Xiaoxi He <xxhe@alauda.io>
2024-12-16 17:56:15 +0800

    Bump k8s version to 0.31.0

--- internal/sample-apiserver/pkg/apiserver/apiserver.go
+++ internal/sample-apiserver/pkg/apiserver/apiserver.go
@@ -21,8 +21,8 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
-	"k8s.io/apimachinery/pkg/version"
 	genericapiserver "k8s.io/apiserver/pkg/server"
+	"k8s.io/apiserver/pkg/util/version"
 )
 
 var (
@@ -34,10 +34,8 @@ var (
 )
 
 func init() {
-
 	// we need to add the options to empty v1
 	// TODO fix the server code to avoid this
-	metav1.AddToGroupVersion(Scheme, schema.GroupVersion{Version: "v1"})
 
 	// TODO: keep the generic API server from wanting this
 	unversioned := schema.GroupVersion{Group: "", Version: "v1"}
@@ -78,14 +76,10 @@ type CompletedConfig struct {
 
 // Complete fills in any fields not set that are required to have valid data. It's mutating the receiver.
 func (cfg *Config) Complete() CompletedConfig {
+	cfg.GenericConfig.Config.EffectiveVersion = version.DefaultBuildEffectiveVersion()
 	c := completedConfig{
 		cfg.GenericConfig.Complete(),
 		&cfg.ExtraConfig,
-	}
-
-	c.GenericConfig.Version = &version.Info{
-		Major: "1",
-		Minor: "0",
 	}
 
 	return CompletedConfig{&c}
